110a9b23e67eed90f585b3838e50cf14337e1c1c
Giloop <giloop.theloop@gmail.com>
2020-03-06 15:16:13 +0100

    modif sur platformio_script.py pour prise en compte de la langue

--- airrohr-firmware/platformio_script.py
+++ airrohr-firmware/platformio_script.py
@@ -2,12 +2,24 @@
 Import("env")
 import os
 import shutil
+import configparser
 from base64 import b64decode
 
-def after_build(source, target, env):
-    # print(ARGUMENTS)
-    print(env.Dump())
-    shutil.copy(target[0].path, "./builds/latest_"+b64decode(ARGUMENTS.get("LANG"))+".bin")
+# uncomment line below to see environment variables
+# print('----\nenv.Dump() : \n------------------------')
+# print(env.Dump())
+# print('----\nARGUMENTS : \n------------------------')
+# print(ARGUMENTS)
+# print(b64decode(ARGUMENTS.get("PROJECT_CONFIG", 1)))
 
+# Get langage in config file (b64decode(ARGUMENTS.get("LANG")) not working)
+config = configparser.ConfigParser()
+config.read(env['PROJECT_CONFIG'])
+lang = config['platformio']['lang']
+
+def after_build(source, target, env):
+  if not os.path.exists("./builds"):
+    os.mkdir("./builds")
+  shutil.copy(target[0].path, "./builds/latest_"+lang+".bin")
 
 env.AddPostAction("$BUILD_DIR/firmware.bin", after_build)
